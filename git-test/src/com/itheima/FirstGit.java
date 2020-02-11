19cd3ffe12bc90ae588268720a3652d65d6de0e4
itYao <1217761398@qq.com>
2020-02-11 11:07:40 +0800

    把本地工程添加到git

---
+++ git-test/src/com/itheima/FirstGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
